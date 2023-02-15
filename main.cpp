e460d35ca7d9f852b08f60b98715afcd4b0ed044
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-14 21:32:53 -0600

    defined NumberList object

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 // Description: Driver program for adjacent ref swapping in linked list for singly linked list and doubly linked list.
 // Date: 2/14/2023
 
+#include "singlyLinkedList.cpp"
+
 int main()
 {
+
+    // Defined a NumberList object.
+    NumberList list;
 
     return 0;
 }
